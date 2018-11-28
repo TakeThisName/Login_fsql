ee00b5a1ffe35c4daab4188df4b3dfb5333e4702
avaaris <avaaris@gmail.com>
2018-11-28 19:44:43 +0100

    Login_fdb_R_w/_link

--- login.php
+++ login.php
@@ -18,8 +18,14 @@
     </head>
     <body>
         <?php
-        if ($res -> num_rows == 1)
-            echo 'Sikeresen bejelenkteztél!  '.$username;
+        if ($res -> num_rows == 1){
+            echo 'Sikeresen bejelenkteztél!';
+            $row = $res -> fetch_row();
+            $_SESSION['userId'] = $row[0];
+            echo '<br><a href="welcome.php">Üdvözlet</a>';
+        }
+        else
+            echo 'Helytelen felhasználónév, vagy jelszó!';
         ?>
     </body>
 </html>
---
+++ welcome.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+
+-->
+<?php
+    session_start();
+    require_once 'connect.php';
+    
+    if (isset($_SESSION['userId'])) {
+       $uid = $_SESSION['userId'];
+    }
+    else {
+     header('Location: hiba.html');
+     die();
+    }
+    
+    $sql = "SELECT uname FROM user WHERE uid=$uid";
+    $res = $db -> query($sql);
+    
+    if (!$res){
+        die();
+    }
+    
+    $row = $res -> fetch_row();
+    $name = $row[0];
+    
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Szép estét!  {$name}";
+        ?>
+    </body>
+</html>
